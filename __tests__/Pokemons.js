cbd4c50b9e297767381c2d68f5144d1c2d14ec81
Santiago García Cabral <stylder@gmail.com>
2021-05-31 01:15:45 -0500

    Try 5

--- __tests__/Pokemons.js
+++ __tests__/Pokemons.js
@@ -5,28 +5,24 @@ import {
   getPokemonSpeciesByName,
 } from '../services/api';
 
-test('Get the pokemon list', async done => {
+test('Get the pokemon list', async () => {
   const {results} = await getPokemonsList({limit: 50, offset: 0});
   expect(results.length).toBe(50);
-  done();
 });
 
-test('Get bulbasaur ', async done => {
+test('Get bulbasaur ', async () => {
   const {name} = await getPokemonByName(1);
   expect(name).toBe('bulbasaur');
-  done();
 });
 
-test('Get bulbasaur image', async done => {
+test('Get bulbasaur image', async () => {
   const image = await getPokemonImage('bulbasaur');
   const isUrl = image.includes('https');
   expect(isUrl).toBe(true);
-  done();
 });
 
-test('Get bulbasaur species', async done => {
+test('Get bulbasaur species', async () => {
   const response = await getPokemonSpeciesByName('bulbasaur');
   expect(response).toHaveProperty('flavor_text_entries');
   expect(response).toHaveProperty('names');
-  done();
 }, 10000);
